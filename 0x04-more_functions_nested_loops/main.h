8d3b92a2816758b487a4a10cddbc0eeb6f339975
obukunmi <solomonbukunmi8@gmail.com>
2023-03-23 16:56:42 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,7 +5,7 @@ int _isupper(int c);
 int _isdigit(int c);
 int _issuport(int c);
 int _isdigit(int c);
-int null(int a; int b);
+int null(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
